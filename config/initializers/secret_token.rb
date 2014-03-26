efc00d5fcade2ab52272b1e4a664a8e9c766faf1
Naz Ut <nazir8ut@gmail.com>
2014-03-26 19:26:03 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NazApp::Application.config.secret_key_base = '223a2b20aebefedfb3052ab3c678a3e6b268faf83da68f23798aadaa833c46a64d231c2d806c389df7e9bf8d402cfa62807e4efc97f482a6ffe5ad8e69ed4c8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NazApp::Application.config.session_store :cookie_store, key: '_naz_app_session'
